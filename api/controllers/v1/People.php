d2aee5208ac228e2648b80493e5d2e277340bd5b
Mary_Glo <mgtambong@gmail.com>
2019-12-09 20:47:21 +0800

    updated function for getting all people collection

--- api/controllers/v1/People.php
+++ api/controllers/v1/People.php
@@ -7,19 +7,19 @@ use Starwars_Test\Util\ArrayUtils;
 use Starwars_Test\Objects\Film as FilmObject;
 
 class People extends Route {
-  private $collection = 'films';
+  private $collection = 'people';
 
-  public function getFilms() {
+  public function getPeople() {
     $db = DbManager::getInstance();
     $conn = $db->getConnection();
     $api = $this->api;
 
     try {
-      $films = $conn->{$this->collection}->find([]);
+      $people = $conn->{$this->collection}->find([]);
 
       return $api->response([
         'success' => true,
-        'films' => json_encode(iterator_to_array($films))
+        'films' => json_encode(iterator_to_array($people))
       ]);
 
     } catch( \MongoDB\Driver\Exception\AuthenticationException $e){
