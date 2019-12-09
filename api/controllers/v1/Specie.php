bd6f149b90483a43896ad6e5eb4a41bfe5475be7
Mary_Glo <mgtambong@gmail.com>
2019-12-09 21:00:30 +0800

    specie controller

---
+++ api/controllers/v1/Specie.php
@@ -0,0 +1,34 @@
+<?php
+namespace Starwars_Test\v1;
+
+use Starwars_Test\Database\DbManager;
+use Starwars_Test\Route;
+use Starwars_Test\Util\ArrayUtils;
+use Starwars_Test\Objects\Species as SpecieObject;
+
+class Specie extends Route {
+  private $collection = 'species';
+
+  public function getPeople() {
+    $db = DbManager::getInstance();
+    $conn = $db->getConnection();
+    $api = $this->api;
+
+    try {
+      $species = $conn->{$this->collection}->find([]);
+
+      return $api->response([
+        'success' => true,
+        'species' => json_encode(iterator_to_array($species))
+      ]);
+
+    } catch( \MongoDB\Driver\Exception\AuthenticationException $e){
+      return $api->response([
+        'success' => false,
+        'species' => array(),
+        'message' => $e->getMessage()
+      ]);
+    }
+  }
+}
+
